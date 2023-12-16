0d989da22b9eaf0134fd0537f0ccba465bc178b3
oclyke <oclyke@oclyke.dev>
2023-12-15 22:27:12 -0800

    switch to moderngl julia set demo

--- main.py
+++ main.py
@@ -1,96 +1,88 @@
-import glfw
-from OpenGL.GL import *
+import numpy as np
+import os
+import moderngl
+import moderngl_window as mglw
 
-# Initialize glfw
-if not glfw.init():
-    raise Exception("glfw cannot be initialized!")
+class Example(mglw.WindowConfig):
+    gl_version = (3, 3)
+    title = "ModernGL Example"
+    window_size = (1280, 720)
+    aspect_ratio = 16 / 9
+    resizable = True
 
-# These hints help to align glfw with the OpenGL version we are using
-# In this case, we are using OpenGL 4.1 on a 2021 Macbook Pro with M1 chip
-glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
-glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 1)
-glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
-glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+    resource_dir = os.path.normpath(os.path.join(__file__, '../data'))
 
-# Creating a window
-window = glfw.create_window(800, 600, "My OpenGL Window", None, None)
-if not window:
-    glfw.terminate()
-    raise Exception("glfw window cannot be created!")
+class Fractal(Example):
+    title = "Julia Set"
+    gl_version = (3, 3)
 
-glfw.make_context_current(window)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-# Create a shader program
-def compile_shader(source, shader_type):
-    shader = glCreateShader(shader_type)
-    glShaderSource(shader, source)
-    glCompileShader(shader)
-    if glGetShaderiv(shader, GL_COMPILE_STATUS) != GL_TRUE:
-        raise RuntimeError(glGetShaderInfoLog(shader))
-    return shader
+        self.prog = self.ctx.program(
+            vertex_shader='''
+                #version 330
 
-vertex_shader = """
-#version 410 core
-layout (location = 0) in vec3 aPos;
+                in vec2 in_vert;
+                out vec2 v_text;
 
-void main()
-{
-    gl_Position = vec4(aPos, 1.0);
-}
-"""
+                void main() {
+                    gl_Position = vec4(in_vert, 0.0, 1.0);
+                    v_text = in_vert;
+                }
+            ''',
+            fragment_shader='''
+                #version 330
 
-fragment_shader = """
-#version 410 core
-out vec4 FragColor;
+                in vec2 v_text;
+                out vec4 f_color;
 
-void main()
-{
-    FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-}
-"""
+                uniform sampler2D Texture;
+                uniform vec2 Seed;
+                uniform int Iter;
 
-shader = glCreateProgram()
-vshader = compile_shader(vertex_shader, GL_VERTEX_SHADER)
-fshader = compile_shader(fragment_shader, GL_FRAGMENT_SHADER)
-glAttachShader(shader, vshader)
-glAttachShader(shader, fshader)
-glLinkProgram(shader)
+                void main() {
+                    vec2 c = Seed;
+                    int i;
+
+                    vec2 z = vec2(3.0 * v_text.x, 2.0 * v_text.y);
+
+                    for (i = 0; i < Iter; i++) {
+                        float x = (z.x * z.x - z.y * z.y) + c.x;
+                        float y = (z.y * z.x + z.x * z.y) + c.y;
+
+                        if ((x * x + y * y) > 4.0) {
+                            break;
+                        }
+
+                        z.x = x;
+                        z.y = y;
+                    }
+
+                    f_color = texture(Texture, vec2((i == Iter ? 0.0 : float(i)) / 100.0, 0.0));
+                }
+            ''',
+        )
+
+        self.seed = self.prog['Seed']
+        self.iter = self.prog['Iter']
+
+        self.texture = self.load_texture_2d('pal.png')
+
+        vertices = np.array([-1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0])
+
+        self.vbo = self.ctx.buffer(vertices.astype('f4'))
+        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert')
+
+    def render(self, time, frame_time):
+        self.ctx.clear(1.0, 1.0, 1.0)
+
+        self.seed.value = (-0.8, 0.156)
+        self.iter.value = 100
+
+        self.texture.use()
+        self.vao.render(moderngl.TRIANGLE_STRIP)
 
 
-# Create VBO (Vertex Buffer Object)
-vertices = [-0.5, -0.5, 0.0, 
-             0.5, -0.5, 0.0, 
-             0.0,  0.5, 0.0]
-vertices = (GLfloat * len(vertices))(*vertices)
-
-vbo = glGenBuffers(1)
-glBindBuffer(GL_ARRAY_BUFFER, vbo)
-glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW)
-
-# Create VAO (Vertex Array Object)
-vao = glGenVertexArrays(1)
-glBindVertexArray(vao)
-glEnableVertexAttribArray(0)
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
-
-# Main loop
-while not glfw.window_should_close(window):
-    glfw.poll_events()
-
-    glClear(GL_COLOR_BUFFER_BIT)
-
-
-    # Use the shader program
-    glUseProgram(shader)
-
-    # Bind the VAO and draw
-    glBindVertexArray(vao)
-    glDrawArrays(GL_TRIANGLES, 0, 3)
-
-    glfw.swap_buffers(window)
-
-# Clean up the shader program
-glDeleteProgram(shader)
-
-# Terminate glfw
-glfw.terminate()
+if __name__ == '__main__':
+    Fractal.run()
